f5db5b6d27e8ba7202aefd87ea14606952d4e9cc
tahany777 <tahanymohammed777@hotmail.com>
2025-01-27 21:07:13 +0000

    home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import data from './components/data/data.json';
-import data1 from './components/data/room.json';
+// import data from './components/data/data.json';
+// import data1 from './components/data/room.json';
 import Home from './components/Home';
 function App() {
-  const [data1, setData1] = useState(data);
-  console.log(data1);
+  // const [data1, setData1] = useState(data);
+  // console.log(data1);
   
   return (
     <div className="App">
